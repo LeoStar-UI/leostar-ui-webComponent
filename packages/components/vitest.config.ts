cee2d5aebebc4e368e0218b0645e86e448299a91
送你十七束花 <108746194+030liuxinghao@users.noreply.github.com>
2023-12-30 18:34:26 +0800

    :rotating_light: remove vitest setup warning

--- packages/components/vitest.config.ts
+++ packages/components/vitest.config.ts
@@ -2,11 +2,11 @@ import { defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
-    viteConfig,
-    defineConfig({
-        test: {
-            environment: "jsdom",
-            setupFiles: "./vitest.setup.ts",
-        },
-    })
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      setupFiles: './vitest.setup.ts',
+    },
+  }),
 )
